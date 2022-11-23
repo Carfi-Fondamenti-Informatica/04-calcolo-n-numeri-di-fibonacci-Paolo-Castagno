87218ad76d387c28ea4e659fdb1f5062061a8ad3
Paolo-Castagno <117978458+Paolo-Castagno@users.noreply.github.com>
2022-11-23 15:34:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+
 #include <iostream>
 using namespace std;
 
 int main() {
-   return 0;
+   int a=1,b=1, c=0, n=0;
+    cin>> n;
+    if (n<2){
+        cout<<"errore";
+    } else {
+    cout << a << endl << b << endl;
+        for (int i=0; i<(n-2); i++){
+            c=a+b;
+            cout<<c<<endl;
+            a=b;
+            b=c;           
+        }
+    }
+    return 0;
 }
